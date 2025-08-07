6a70a924adde76848d64e2d14c00b201dc9e213f
Darkpwd <natanyuri1010@gmail.com>
2025-08-07 17:22:23 -0300

    feat(create-gym): create gym controller

--- src/http/controllers/gyms/create-gym.controller.ts
+++ src/http/controllers/gyms/create-gym.controller.ts
@@ -1,11 +1,10 @@
-import { EmailAlreadyExists } from '@/use-cases/errors/email-already-exists-error'
 import { makeCreateGymUseCase } from '@/use-cases/factories/make-create-gym-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
 export async function create(request: FastifyRequest, reply: FastifyReply) {
   const createGymBodySchema = z.object({
-    title: z.string().nonempty(),
+    title: z.string(),
     description: z.string().nullable(),
     phone: z.string().nullable(),
     latitude: z.number().refine((value) => {
@@ -19,25 +18,15 @@ export async function create(request: FastifyRequest, reply: FastifyReply) {
   const { title, description, phone, latitude, longitude } =
     createGymBodySchema.parse(request.body)
 
-  try {
-    const registerUseCase = makeCreateGymUseCase()
+  const createGymUseCase = makeCreateGymUseCase()
 
-    await registerUseCase.execute({
-      title,
-      description,
-      phone,
-      latitude,
-      longitude,
-    })
-  } catch (error) {
-    if (error instanceof EmailAlreadyExists) {
-      return reply.status(409).send({
-        message: error.message,
-      })
-    }
-
-    throw error
-  }
+  await createGymUseCase.execute({
+    title,
+    description,
+    phone,
+    latitude,
+    longitude,
+  })
 
   return reply.status(201).send()
 }
